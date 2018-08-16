2b47f22ccb216fc097974d354202b3f0dfcee2e1
gwonyg <cristal_inca@hanmail.net>
2018-08-16 13:57:34 +0900

    first

---
+++ config/sample_seoul_key.js
@@ -0,0 +1,6 @@
+var seoul_api = {
+		api_key : "736f70466e67776f3932636e477363"
+		api_url : "http://openAPI.seoul.go.kr:8088/",
+		api_name : "SearchPerformanceBySubjectService"
+}
+exports.seoul = seoul_api
